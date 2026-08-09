053eb68796a223d157d4dfcabb6a29008d6a6f52
Yasemin Türkoğlu <126945023+fizikzedenin-kodlari@users.noreply.github.com>
2026-08-09 14:52:03 +0300

    Add files via upload

---
+++ 3.video_jasminlean.lean
@@ -0,0 +1,7 @@
+import Mathlib
+Whitespaces
+In Lean 4, sometimes you need whitespaces. Not everytime.
+--example: (x : ) you need it
+--example: a + b =a+b both are true
+example (a b: ℝ): a+b = b +                  a := by sorry
+Goodbye!
