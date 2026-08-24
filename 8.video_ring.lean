14f87ceec1047dd8559fb883f083ce665ef35fec
Yasemin Türkoğlu <126945023+fizikzedenin-kodlari@users.noreply.github.com>
2026-08-24 12:19:33 +0300

    Add files via upload

---
+++ 8.video_ring.lean
@@ -0,0 +1,14 @@
+import Mathlib
+variable (R : Type*) [CommRing R]
+variable (a b c d : R)
+----------------------------------------------
+example : c * b * a = b * (a * c) := by ring
+----------------------------------------------
+example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b := by ring
+----------------------------------------------
+example : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by ring
+----------------------------------------------
+example (hyp : c = d * a + b) (hyp' : b = a * d) : c = 2 * a * d := by
+  rw [hyp, hyp']
+  ring
+----------------------------------------------
