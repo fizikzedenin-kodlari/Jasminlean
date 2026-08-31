9b59c4aea8cdadd169ba4e56c6293aec74cf6717
Yasemin Türkoğlu <126945023+fizikzedenin-kodlari@users.noreply.github.com>
2026-08-31 07:10:05 +0300

    Add files via upload

---
+++ 9.video_myRing.lean
@@ -0,0 +1,12 @@
+import Mathlib
+namespace MyRing
+variable {R : Type*} [Ring R]  
+
+theorem add_zero (a : R) : a + 0 = a := by rw [add_comm, zero_add]
+
+theorem add_right_neg (a : R) : a + -a = 0 := by rw [add_comm, neg_add_cancel]
+
+#check MyRing.add_zero
+#check add_zero
+
+end MyRing
